90cd6ff10428bfe5d3b34d0557979317f0c9692d
Daveonwave <davide.salaorni@polimi.it>
2024-06-06 18:21:50 +0200

    readme and folder structure

--- launch_env.py
+++ launch_env.py
@@ -1,9 +1,9 @@
 import argparse
 
-from ernestogym.envs.single_agent.ppo import run_ppo
-from ernestogym.envs.single_agent.a2c import run_a2c
-from ernestogym.envs.single_agent.sac import run_sac
-from ernestogym.envs.single_agent.baselines import random_action_policy, deterministic_action_policy
+from ernestogym.algorithms.single_agent.ppo import run_ppo
+from ernestogym.algorithms.single_agent.a2c import run_a2c
+from ernestogym.algorithms.single_agent.sac import run_sac
+from ernestogym.algorithms.single_agent.baselines import random_action_policy, deterministic_action_policy
 
 
 def get_args():
@@ -20,11 +20,11 @@ if __name__ == '__main__':
     # run_a2c()
     # run_sac()
 
-    #random_action_policy()
-    #deterministic_action_policy(action=0., filename="only_market")
-    #deterministic_action_policy(action=1., filename="battery_first")
-    #deterministic_action_policy(action=0.2, filename="20-80")
-    #deterministic_action_policy(action=0.5, filename="50-50")
+    # random_action_policy()
+    # deterministic_action_policy(action=0., filename="only_market")
+    # deterministic_action_policy(action=1., filename="battery_first")
+    # deterministic_action_policy(action=0.2, filename="20-80")
+    # deterministic_action_policy(action=0.5, filename="50-50")
 
     run_ppo()
     run_a2c()
