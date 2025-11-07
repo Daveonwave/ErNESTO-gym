a5ff1d8d7ccdecd6e6d60c1486a48df059db9a16
Andrea-Barisione <andrea.barisione@rse-web.it>
2025-11-07 16:25:46 +0100

    Bash files with different seeds

---
+++ scripts/ppo_new_args_seed_42.sh
@@ -0,0 +1,29 @@
+python launch_env.py \
+    --algo ppo \
+    --n_envs 10 \
+    --exp_name "PPO_experiment" \
+    --spread_factor 1 \
+    --save_results_as "ppo_rse" \
+    --gamma 0.99 \
+    --n_episodes 5000 \
+    --policy_network 64 32 \
+    --log_std_init -1 \
+    --batch_size 256 \
+    --n_steps 4096 \
+    --n_epochs 10 \
+    --clip_range 0.2 \
+    --gae_lambda 0.95 \
+    --ent_coef 0.0 \
+    --vf_coef 0.5 \
+    --max_grad_norm 0.5 \
+    --learning_rate 0.00005 \
+    --log_rate 5 \
+    --eval_freq 672 \
+    --n_eval_episodes 1 \
+    --seed 42 \
+    --world_settings ernestogym/envs/single_agent/ijcnn_deg_train_cell.yaml \
+    --electrical_model ernestogym/ernesto/data/battery/models/electrical/thevenin_cell.yaml \
+    --thermal_model ernestogym/ernesto/data/battery/models/thermal/r2c_thermal_cell.yaml\
+    --aging_model ernestogym/ernesto/data/battery/models/aging/bolun_cell.yaml \
+    --eval_world_settings ernestogym/envs/single_agent/ijcnn_deg_test_cell.yaml \
+    --train
